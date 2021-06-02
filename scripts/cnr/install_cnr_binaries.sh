f46689c04518b946c6822b274f191608a095581d
Ubuntu <ubuntu@ip-172-31-0-41.us-east-2.compute.internal>
2021-06-02 23:52:43 +0000

    remove unwanted binaries

--- scripts/cnr/install_cnr_binaries.sh
+++ scripts/cnr/install_cnr_binaries.sh
@@ -13,10 +13,10 @@ wait_for_cloudinit
 ### Juniper ##################################################################
 ################################################################################
 echo "### Juniper ##################################################################"
-echo "### Copy Platter files ##################################################################"
-wget https://jnpr-cnr.s3.us-east-2.amazonaws.com/platter-arm64-20210513-1d6ecc2.tgz
-wget https://jnpr-cnr.s3.us-east-2.amazonaws.com/crpd-arm64-21.3I20210427_1631.docker.tgz
-echo "### Copy Platter files ##################################################################"
+#echo "### Copy Platter files ##################################################################"
+#wget https://jnpr-cnr.s3.us-east-2.amazonaws.com/platter-arm64-20210513-1d6ecc2.tgz
+#wget https://jnpr-cnr.s3.us-east-2.amazonaws.com/crpd-arm64-21.3I20210427_1631.docker.tgz
+#echo "### Copy Platter files ##################################################################"
 echo "### Linux extras #########################################"
 version=`uname -r`
 sudo apt-get install -y tcpdump jq ethtool tmux linux-modules-extra-$version
